9cabe1a8fcbcc029e05cf3aecd910c3ede3a1d28
Alexander Smit <alexsmit89@gmail.com>
2013-06-06 13:33:55 +0000

    Change 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
 
-void main()
+int main(int argc, char *argv[])
 {
+	return 0;
 }
 
